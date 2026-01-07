a13d463b4fd0b457824d30e2bf8ad8099a0425fe
jake <epikeran@gmail.com>
2026-01-07 18:41:34 +0900

    feat : Add message sequence id in client

--- message-system-client/src/main/java/com/jake/messagesystem/dto/websocket/inbound/MessageNotification.java
+++ message-system-client/src/main/java/com/jake/messagesystem/dto/websocket/inbound/MessageNotification.java
@@ -4,22 +4,34 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.jake.messagesystem.constants.MessageType;
 import com.jake.messagesystem.dto.ChannelId;
+import com.jake.messagesystem.dto.MessageSeqId;
 
 public class MessageNotification extends BaseMessage {
     private final ChannelId channelId;
+    private final MessageSeqId messageSeqId;
     private final String username;
     private final String content;
 
     @JsonCreator
-    public MessageNotification(@JsonProperty("channelId") ChannelId channelId, @JsonProperty("username") String username, @JsonProperty("content") String content) {
+    public MessageNotification(
+            @JsonProperty("channelId") ChannelId channelId,
+            @JsonProperty("messageSeqId") MessageSeqId messageSeqId,
+            @JsonProperty("username") String username,
+            @JsonProperty("content") String content
+    ) {
         super(MessageType.NOTIFY_MESSAGE);
         this.channelId = channelId;
+        this.messageSeqId = messageSeqId;
         this.username = username;
         this.content = content;
     }
 
     public ChannelId getChannelId() {
         return channelId;
+    }
+
+    public MessageSeqId getMessageSeqId() {
+        return messageSeqId;
     }
 
     public String getUsername() {
