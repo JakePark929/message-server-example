a8b2b4b8fa0a16720fee83bfe96442687e9e73c0
jake <epikeran@gmail.com>
2026-01-15 14:37:24 +0900

    feat : Add database sharding structure

--- message-system-server/src/main/java/com/jake/messagesystem/db/RoutingDataSource.java
+++ message-system-server/src/main/java/com/jake/messagesystem/db/RoutingDataSource.java
@@ -11,6 +11,11 @@ public class RoutingDataSource extends AbstractRoutingDataSource {
     @Override
     protected Object determineCurrentLookupKey() {
         String dataSourceKey = TransactionSynchronizationManager.isCurrentTransactionReadOnly() ? "replica" : "source";
+        Long channelId = ShardContext.getChannelId();
+        if (channelId != null) {
+            dataSourceKey += channelId % 2 == 0 ? "Message2" : "Message1";
+        }
+
         log.info("Routing to {} DataSource.", dataSourceKey);
 
         return dataSourceKey;
